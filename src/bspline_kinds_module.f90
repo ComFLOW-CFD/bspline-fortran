404ff8567370705c922e03db2b9c3e5a7ea85a0f
Bas van 't Hof <bas@vortech.nl>
2024-01-26 14:26:29 +0100

    Correction.

--- src/bspline_kinds_module.f90
+++ src/bspline_kinds_module.f90
@@ -5,9 +5,9 @@
 !### Description
 !  Numeric kind definitions for BSpline-Fortran.
 
-    module bspline_kinds_module, only: real64
+    module bspline_kinds_module
 
-    use,intrinsic :: iso_fortran_env
+    use,intrinsic :: iso_fortran_env, only: real64 
 
     implicit none
 
